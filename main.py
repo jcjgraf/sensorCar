f64a5c3ababf4ee4ddb1a79a476b75e68416edec
Jean-Claude Graf <jeanggi90@gmail.com>
2018-05-11 19:10:20 +0200

    Wrote a performance function which calculates a deltaError based on inputs and associated lables. Furthermore I changed all "dataset" to "dataSet" and "Dataset" to "DataSet".

--- main.py
+++ main.py
@@ -20,42 +20,42 @@ class SensorCarNetwork():
 		net
 	"""
 
-	def __init__(self, networkShape, datasetPath=False, datasetInputLabelRatio=False):
+	def __init__(self, networkShape, dataSetPath=False, dataSetInputLabelRatio=False):
 		"""
-			Init FullyConnected network and if a datasetPath and datasetInputLabelRatio is
+			Init FullyConnected network and if a dataSetPath and dataSetInputLabelRatio is
 			provided, also a DataSet
 		"""
 
 		self.net = FullyConnected(networkShape)
 
-		if datasetPath and datasetInputLabelRatio:
+		if dataSetPath and dataSetInputLabelRatio:
 
-			self.dataset = DataSet(datasetPath, datasetInputLabelRatio)
+			self.dataSet = DataSet(dataSetPath, dataSetInputLabelRatio)
 
 	def prepareForTraining(self):
 		"""
-			Prepare the dataset if not already done. If no dataset is initialized,
+			Prepare the dataSet if not already done. If no dataSet is initialized,
 			false is returned, else true
 		"""
 
-		# If no dataset is initialized, return
-		if self.dataset is None:
+		# If no dataSet is initialized, return
+		if self.dataSet is None:
 			return False
 
-		self.dataset.prepareDataset()
+		self.dataSet.prepareDataSet()
 
 		return True
 
 	def trainNetwork(self, epochs=1):
 		"""
-			Train network, buto nly if a dataset is initialized. An optional
+			Train network, but only if a dataSet is initialized. An optional
 			epochs parameter can be given
 		"""
 
-		# If no dataset is initialized, training is not possible
-		if self.prepareForTraining() is False:
-			print("No dataset initialized. Training not possible")
-			return
+		# If no dataSet is initialized, training is not possible
+		# if self.prepareForTraining() is False:
+		# 	print("No dataSet initialized. Training not possible")
+		# 	return
 
 		print("Training network")
 
@@ -66,7 +66,7 @@ class SensorCarNetwork():
 			# Get the training entities line by line and train the network with them
 			line = 1  # Starts with index 1 not 0
 
-			lineEntities = self.dataset.getInputLableEntity(line)
+			lineEntities = self.dataSet.getInputLableEntity(line, self.dataSet.trainingDataSetPath)
 
 			# While we are not at the end of the list
 			while lineEntities is not False:
@@ -75,19 +75,59 @@ class SensorCarNetwork():
 
 				line += 1
 
-				lineEntities = self.dataset.getInputLableEntity(line)
+				lineEntities = self.dataSet.getInputLableEntity(line, self.dataSet.trainingDataSetPath)
 
 		print("Finished training")
 
+	def getPerformance(self):
+		"""
+			Calculates the performance of the network
+		"""
+
+		with open(self.dataSet.testDataSetPath, "r") as f:
+
+			differences = []
+
+			for line in f:
+
+				# An error is thrown when there is an empty line in the dataSet
+				try:
+
+					# split line entites into inputs and labels array
+					lineEntities = np.array([float(i) for i in line.split("\t")])
+					inputs = lineEntities[:self.dataSet.inputLabelRatio[0]]
+					labels = lineEntities[-self.dataSet.inputLabelRatio[1]:]
+
+					# Evaluate inputs
+					outputs = self.evaluateMetering(inputs)
+
+					differences.append(np.subtract(outputs, labels))
+
+				except Exception as e:
+					raise e
+
+			deltaError = np.average(np.array(differences))
+
+			print("dError", deltaError)
+
 	def evaluateMetering(self, sensorInputs):
+		"""
+			Takes the sensorInputs vector and returns the evaluated output layer
+			vector
+		"""
 
 		return self.net.evaluate(np.array(sensorInputs))
 
 
 if __name__ == '__main__':
 
-	path = "./simulation/dataset/track636613955649037100.txt"
+	path = "./simulation/dataSet/track636613955649037100.txt"
 
 	sensorCar = SensorCarNetwork([3, 10, 1], path, [3, 1])
+	sensorCar.dataSet.generateTrainingTestSets([9, 1])
+
+	sensorCar.getPerformance()
 
 	sensorCar.trainNetwork()
+
+	sensorCar.getPerformance()
