b3b94286f20f664ebcfdf40c0c8956efe1503683
Jean-Claude Graf <jeanggi90@gmail.com>
2018-05-12 23:48:53 +0200

    Created a socketClient and added it to the SocketIO gameObject. The Class handles events for open, close and error. Working on the sending of data next

---
+++ simulation/sensorCar/Assets/Scripts/SocketClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using SocketIO;
+
+public class SocketClient : MonoBehaviour {
+
+	private SocketIOComponent socket;
+
+	public void Start()  {
+
+		socket = GetComponent<SocketIOComponent>();
+
+		socket.On("open", onOpen);
+		socket.On("error", onError);
+		socket.On("close", onClose);
+		socket.On("receive", onSteer);
+		
+	}
+
+	public void Update() {
+
+
+	}
+
+	public void onOpen(SocketIOEvent obj) {
+		Debug.Log("[SocketIO] Open received: " + obj.name + " " + obj.data);
+	}
+	
+	public void onError(SocketIOEvent obj) {
+		Debug.Log("[SocketIO] Error received: " + obj.name + " " + obj.data);
+	}
+	
+	public void onClose(SocketIOEvent obj) {	
+		Debug.Log("[SocketIO] Close received: " + obj.name + " " + obj.data);
+	}
+
+	public void onSteer(SocketIOEvent obj) {
+		Debug.Log("[SocketIO] Open received: " + obj.name + " " + obj.data);
+		
+		// TODO Steer car
+	}
+}
