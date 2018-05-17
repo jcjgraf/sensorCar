f29dff0eae7d2e30d14f7c6070c9bd9b29d0e583
Jean-Claude Graf <jeanggi90@gmail.com>
2018-05-18 00:07:45 +0200

    Added a netMove method through which it should be possible to move the car with a steeringangle returned by the net

--- simulation/sensorCar/Assets/Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ simulation/sensorCar/Assets/Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -39,7 +39,7 @@ namespace UnityStandardAssets.Vehicles.Car
 
         private Quaternion[] m_WheelMeshLocalRotations;
         private Vector3 m_Prevpos, m_Pos;
-        private float m_SteerAngle;
+        [SerializeField] float m_SteerAngle;
         private int m_GearNum;
         private float m_GearFactor;
         private float m_OldRotation;
@@ -146,6 +146,53 @@ namespace UnityStandardAssets.Vehicles.Car
             //Set the steer on the front wheels.
             //Assuming that wheels 0 and 1 are the front wheels.
             m_SteerAngle = steering*m_MaximumSteerAngle;
+            m_WheelColliders[0].steerAngle = m_SteerAngle;
+            m_WheelColliders[1].steerAngle = m_SteerAngle;
+
+            SteerHelper();
+            ApplyDrive(accel, footbrake);
+            CapSpeed();
+
+            //Set the handbrake.
+            //Assuming that wheels 2 and 3 are the rear wheels.
+            if (handbrake > 0f)
+            {
+                var hbTorque = handbrake*m_MaxHandbrakeTorque;
+                m_WheelColliders[2].brakeTorque = hbTorque;
+                m_WheelColliders[3].brakeTorque = hbTorque;
+            }
+
+
+            CalculateRevs();
+            GearChanging();
+
+            AddDownForce();
+            CheckForWheelSpin();
+            TractionControl();
+        }
+
+        // NetMove
+        public void netMove(float steeringAngle, float accel, float footbrake, float handbrake) {
+            
+            Debug.Log("1");
+            for (int i = 0; i < 4; i++) {
+                Quaternion quat;
+                Vector3 position;
+                m_WheelColliders[i].GetWorldPose(out position, out quat);
+                m_WheelMeshes[i].transform.position = position;
+                m_WheelMeshes[i].transform.rotation = quat;
+            }
+
+            //clamp input values
+            // steering = Mathf.Clamp(steering, -1, 1);
+            AccelInput = accel = Mathf.Clamp(accel, 0, 1);
+            BrakeInput = footbrake = -1*Mathf.Clamp(footbrake, -1, 0);
+            handbrake = Mathf.Clamp(handbrake, 0, 1);
+
+            //Set the steer on the front wheels.
+            //Assuming that wheels 0 and 1 are the front wheels.
+            // m_SteerAngle = steering*m_MaximumSteerAngle;
+            m_SteerAngle = steeringAngle;
             m_WheelColliders[0].steerAngle = m_SteerAngle;
             m_WheelColliders[1].steerAngle = m_SteerAngle;
 
