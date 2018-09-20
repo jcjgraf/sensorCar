0ee43503bb122b47b3023e4d60e25dcac1043467
Jean-Claude Graf <jeanggi90@gmail.com>
2018-09-20 23:40:00 +0200

    Restructured main.py in order to make it more user friendly

--- main.py
+++ main.py
@@ -14,27 +14,25 @@ from dataSet import DataSet
 from sensorCarController import SensorCarController
 from activationFunction import ActivationFunction
 
+
+def runCar():
+	networkPath = "../neuralNetworks/savedNet/3-100-50-10-1-0_001/100.txt"
+
+	network = Network()
+	network.loadNet(networkPath)
+	sensorCarController = SensorCarController(network)
+	sensorCarController.startServer()
+
+
+def trainNetwork():
+	dataSetPath = "./simulation/dataset/trackMaster.txt"
+	hiddenLayerShape = []
+
+	dataSet = DataSet(dataSetPath, [3, 1])
+	network = Network([3] + hiddenLayerShape + [1], ActivationFunction.tanh, dataSet)
+	network.train(epochs=1, learningRate=0.3, verbosity=1, saveNet=5)
+
+
 if __name__ == '__main__':
-
-	# Load network
-	if (True):
-		network = Network([3, 100, 50, 10, 1], ActivationFunction.tanh)
-
-		network.loadNet("../neuralNetworks/savedNet/3-100-50-10-1-0_001/100.txt")
-
-		sensorCarController = SensorCarController(network)
-
-		sensorCarController.startServer()
-
-	# Creat network and train
-	if (False):
-
-		dataSet = DataSet("./simulation/dataset/trackMaster.txt", [3, 1])
-
-		network = Network([3, 3, 1], ActivationFunction.tanh, dataSet)
-
-		network.train(epochs=1, learningRate=0.3, verbosity=1)
-
-		sensorCarController = SensorCarController(network)
-
-		sensorCarController.startServer()
+	
+	runCar()
