65256bdbd756da29430fa08900246404a3a49af4
Jean-Claude Graf <jeanggi90@gmail.com>
2018-09-20 23:41:03 +0200

    Created a line renderer which is capable of drawing the path of the car

---
+++ simulation/sensorCar/Assets/Scripts/helper/DrawLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+	[SerializeField] private GameObject lineGeneratorPrefab;
+	[SerializeField] private Transform car;
+	[SerializeField] private float deltaRecordTime;
+
+	private List<Vector3> pointPositions;
+
+	private GameObject lineGenerator;
+	private LineRenderer lineRenderer;
+
+
+	private void Start() {
+		lineGenerator = Instantiate (lineGeneratorPrefab);
+		lineRenderer = lineGenerator.GetComponent<LineRenderer> ();
+
+		pointPositions = new List<Vector3> ();
+
+		InvokeRepeating("drawLineSegment", 0, deltaRecordTime);
+	}
+
+	private void drawLineSegment() {
+		
+		pointPositions.Add (car.position);
+
+		lineRenderer.positionCount = pointPositions.Count;
+
+		lineRenderer.SetPosition (pointPositions.Count - 1, car.position);
+	}
+}
