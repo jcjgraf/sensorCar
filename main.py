09645af19e78f346e663aaad5856e51b66cdc7ae
Jean-Claude Graf <jeanggi90@gmail.com>
2018-09-20 13:05:33 +0200

    Added comments and two cases, one for loadeng a network, and one for creating, training and saving a network

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python3
 
 """
-	Main script of the sensorCar project
+	Main script of the sensorCar project. Interpreting this file will start up
+	the socket server, neural network etc. Depending on whether we need to load,
+	save etc. a trained network this file has to be altered
 """
 
 import sys
@@ -9,19 +11,30 @@ sys.path.insert(0, '../neuralNetworks/')
 
 from network import Network
 from dataSet import DataSet
-
 from sensorCarController import SensorCarController
-
+from activationFunction import ActivationFunction
 
 if __name__ == '__main__':
 
-	path = "/Users/JeanClaude/Documents/Programming/matriculationProject/practical/sensorCar/simulation/dataSet/trackMaster.txt"
+	# Load network
+	if (True):
+		network = Network([3, 100, 50, 10, 1], ActivationFunction.tanh)
 
-	dataSet = DataSet(path, [3, 1], [1, 1])
-	network = Network([3, 100, 50, 10, 1], dataSet)
+		network.loadNet("../neuralNetworks/savedNet/3-100-50-10-1-0_001/100.txt")
 
-	network.loadNet("../neuralNetworks/savedNet/3-100-50-10-1-0_001/100.txt")
+		sensorCarController = SensorCarController(network)
 
-	sensorCarController = SensorCarController(network)
+		sensorCarController.startServer()
 
-	sensorCarController.startServer()
+	# Creat network and train
+	if (False):
+
+		dataSet = DataSet("./simulation/dataset/trackMaster.txt", [3, 1])
+
+		network = Network([3, 3, 1], ActivationFunction.tanh, dataSet)
+
+		network.train(epochs=1, learningRate=0.3, verbosity=1)
+
+		sensorCarController = SensorCarController(network)
+
+		sensorCarController.startServer()
