605e2ef76924b59d7a61280d99ea258ad89a9458
Jean-Claude Graf <jeanggi90@gmail.com>
2019-01-29 19:05:03 +0100

    Finished the training method of networkTF and added all verbosity related code. Adapted main and sensorCarController to make them work in conjunction with networkTF which

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 """
 
 from network import Network
+from networkTF import NetworkTF
 from dataSet import DataSet
 from sensorCarController import SensorCarController
 from activationFunction import ActivationFunction
@@ -16,10 +17,11 @@ from multiprocessing import Pool
 
 def runCar(networkPath):
 
-	network = Network()
-	network.loadNet(networkPath)
+	network = NetworkTF([3, 100, 50, 10, 1])
+	network.doLoad(networkPath)
+	# network.loadNet(networkPath)
 	# todo temporarly fix
-	network.dff.activation = ActivationFunction.tanh
+	# network.dff.activation = ActivationFunction.tanh
 	sensorCarController = SensorCarController(network)
 	sensorCarController.startServer()
 
@@ -31,12 +33,15 @@ def processTrainNetwork(data):
 def trainNetwork(hiddenLayerShape, learningRate, dataSetPath, epochs):
 
 	dataSet = DataSet(dataSetPath, [3, 1])
-	network = Network([3] + hiddenLayerShape + [1], ActivationFunction.tanh, dataSet)
-	network.train(epochs=epochs, learningRate=learningRate, verbosity=10, saveNet=10)
+
+	# network = Network([3] + hiddenLayerShape + [1], ActivationFunction.tanh, dataSet)
+	network = NetworkTF([3] + hiddenLayerShape + [1], dataSet)
+	# network.train(epochs=epochs, learningRate=learningRate, verbosity=10, saveNet=10)
+	network.train(epochs=epochs, learningRate=learningRate, saveStep=10)
 
 
 if __name__ == '__main__':
 
-	# trainNetwork([20], 0.03, "./simulation/dataset/trackMaster.txt", 100)
+	# trainNetwork([100, 50, 10], 0.03, "./simulation/dataset/trackMaster1k.txt", 1)
 
-	runCar("./savedNet/3-100-50-10-1-0_001-trackMaster/440.txt")
+	runCar("./savedNetTF/3-100-50-10-1-0_03-trackMaster1k/model-0")
--- networkTF.py
+++ networkTF.py
@@ -7,6 +7,7 @@ import functools
 import numpy as np
 import tensorflow as tf
 import os
+import time
 
 class NetworkTF:
 
@@ -26,10 +27,10 @@ class NetworkTF:
 	savePath = None
 
 
-	def __init__(self, shape, dataSet=False):
+	def __init__(self, shape, dataSet=None):
 		self.shape = shape
 
-		if dataSet is not False:
+		if dataSet is not None:
 			self.dataSet = dataSet
 
 		# Tensorflow attributes
@@ -47,29 +48,98 @@ class NetworkTF:
 		# self.optimizer
 		self.loss
 
-	def train(self, epochs=1, learningRate=0.3, printStep=None, saveStep=None):
+	def train(self, epochs=1, learningRate=0.3, verbosity=1, saveStep=None):
+
+		# Check if training possible
+		if self.dataSet is None:
+			print("Training not possbile since no dataSet is assigned")
+			return
+
+		print("{0}\nTraining started\nnumberOfEpochs: {1}".format(15 * "-", epochs))
 
 		if saveStep is not None:
-			self.savePath = "./savedNetTF/" + "".join(str(e) + "-" for e in self.shape) + str(learningRate).replace('.', '_') + "/"
+			ds = self.dataSet.fullDataSetPath
+
+			self.savePath = "./savedNetTF/" + "".join(str(e) + "-" for e in self.shape) + str(learningRate).replace('.', '_') + "-" + ds[ds.rfind("/") + 1: ds.rfind(".")] + "/"
+
+			while os.path.exists(self.savePath):
+				self.savePath = self.savePath[:self.savePath.rfind("/")] + "I" + self.savePath[self.savePath.rfind("/"):]
+
+			os.makedirs(self.savePath)
+
 			self.saver
 
-		for step in range(epochs):
+		startTrainingTime = time.time()  # Used for calculating used time
+		costList = []  # Holds the cost value of each epoch
+
+		deltaEpochTrainingTime = 0  # time between two epochs
+
+		deltaPrintTrainingTime = 0  # time between two prints
+		previousPrintTrainingTime = time.time()  # time of the previous print. Used for calculating the deltaPrintTime
+
+		deltaEpochCost = 0
+		previousEpochCost = 0
+
+		deltaPrintCost = 0
+		previousPrintCost = 0
+
+		for epoch in range(epochs):
 
 			with open(self.dataSet.trainingDataSetPath, "r") as trf:
 
-				for line in trf:
-					lineEntities = np.array([float(i) for i in line.split(",")], dtype=np.float128)
+				costSum = 0  # costSum of one epoch
+				numberOfLines = 0
 
-					inputs = lineEntities[:self.dataSet.inputLabelNumber[0]]
-					labels = np.divide(lineEntities[-self.dataSet.inputLabelNumber[1]:], 25)
+				startEpochTrainingTime = time.time()
+
+				doPrint = False
+
+				# Determine whether this epoch data should be printed
+				if verbosity is not 0 and ((epoch + 1) % verbosity == 0 or epoch == 0):
+
+					print("{2}\nEpoch {0}/{1}".format(epoch + 1, epochs, 15 * "-"))
+
+					doPrint = True
+
+				for line in trf:
+					print("Process line")
+					lineEntities = np.array([float(i) for i in line.split(",")], dtype=np.float128)
+					inputs = np.array(lineEntities[:self.dataSet.inputLabelNumber[0]], ndmin=2)
+					labels = np.array(np.divide(lineEntities[-self.dataSet.inputLabelNumber[1]:], 25), ndmin=2)
 
 					self.sess.run(self.optimizer(learningRate), feed_dict={self.x: inputs, self.y: labels})
 
-			if printStep and (step == 0 or step == epochs or step % printStep == 0):
-				print(step, self.sess.run(self.loss, feed_dict={self.x: xData, self.y: yData}))
+					costSum += self.sess.run(self.loss, feed_dict={self.x: inputs, self.y: labels})
 
-			if saveStep is not None and ((step == 0) or (step == epochs - 1) or step % saveStep == 0):
-				self.doSave(step)
+					numberOfLines += 1
+
+			costList.append(costSum / numberOfLines)
+
+			cost = costSum / numberOfLines
+
+			deltaEpochCost = previousEpochCost - cost
+			previousEpochCost = cost
+
+			if doPrint:
+
+				deltaTrainingTime = time.time() - startTrainingTime
+
+				deltaEpochTrainingTime = time.time() - startEpochTrainingTime
+
+				deltaPrintTrainingTime = time.time() - previousPrintTrainingTime
+				previousPrintTrainingTime = time.time()
+
+				deltaPrintCost = previousPrintCost - cost
+				previousPrintCost = cost
+
+				print("{0}{1}{0}".format(5 * "-", self.shape))
+				print("deltaTrainingTime:\t{},\ndeltaEpochTrainingTime:\t{},\ndeltaPrintTrainingTime:\t{},\ncost:\t{},\ndeltaEpochCost:\t{},\ndeltaPrintCost:\t{}".format(deltaTrainingTime, deltaEpochTrainingTime, deltaPrintTrainingTime, cost, deltaEpochCost, deltaPrintCost))
+
+			# if printStep and (step == 0 or step == epochs or step % printStep == 0):
+			# 	print(step, self.sess.run(self.loss, feed_dict={self.x: xData, self.y: yData}))
+
+			if saveStep is not None and ((epoch == 0) or (epoch == epochs - 1) or epoch % saveStep == 0):
+				self.doSave(epoch)
 
 	def getPrediction(self, xData):
 		return self.sess.run(self.predict, feed_dict={self.x: xData})
@@ -97,16 +167,12 @@ class NetworkTF:
 	@propertyWithCheck
 	def saver(self):
 
-		while os.path.exists(self.savePath):
-			self.savePath = self.savePath[:self.savePath.rfind("/")] + "I" + self.savePath[self.savePath.rfind("/"):]
-
-		os.makedirs(self.savePath)
-
-
 		return tf.train.Saver(max_to_keep=10000)
 
-	def evaluate(self, xData, yData):
-		return self.sess.run(self.loss, feed_dict={self.x: xData, self.y: yData})
+	# def evaluate(self, xData, yData):
+		# return self.sess.run(self.loss, feed_dict={self.x: xData, self.y: yData})
+	def evaluate(self, xData):
+		return self.getPrediction(xData)
 
 	def doSave(self, step):
 		savePath = self.saver.save(self.sess, os.path.join(self.savePath, "model"), global_step = step)
